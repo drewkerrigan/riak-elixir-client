5a8916b8a0c9ee30d656f0e5385edbcd3b8fd065
drewkerrigan <dkerrigan@basho.com>
2015-03-05 17:24:13 -0700

    updating pooler dep to use hex

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Riak.Mixfile do
     [ {:earmark, only: :dev},
       {:ex_doc, only: :dev},
       {:linguist, "~> 0.1"},
-      {:pooler, github: "seth/pooler", tag: "1.3.3"},
+      {:pooler, "1.4.0"},
       {:meck, github: "eproxus/meck", tag: "0.8.2", override: true},
       {:riak_pb, github: "basho/riak_pb", override: true, tag: "2.0.0.16", compile: "./rebar get-deps compile deps_dir=../"},
       {:riakc, github: "basho/riak-erlang-client", tag: "2.0.1"} ]

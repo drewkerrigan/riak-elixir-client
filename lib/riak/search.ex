2ef8a5747eac2552fa5e9ee095c1fa464caa2509
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-02-23 23:53:10 +0100

    fixing naming of index fields

--- lib/riak/search.ex
+++ lib/riak/search.ex
@@ -2,24 +2,24 @@ defmodule Riak.Search do
   import Riak.Pool
   import :riakc_pb_socket, except: [put: 2, put: 3, put: 4]
 
-  defpool query(pid, bucket, query) when is_pid(pid), do: search(pid, bucket, query)
-  defpool query(pid, bucket, query, options) when is_pid(pid), do: search(pid, bucket, query, options)
-  defpool query(pid, bucket, query, options, timeout) when is_pid(pid), do: search(pid, bucket, query, options, timeout)
+  defpool query(pid, index, query) when is_pid(pid), do: search(pid, index, query)
+  defpool query(pid, index, query, options) when is_pid(pid), do: search(pid, index, query, options)
+  defpool query(pid, index, query, options, timeout) when is_pid(pid), do: search(pid, index, query, options, timeout)
 
   defmodule Index do
     defpool list(pid) when is_pid(pid), do: list_search_indexes(pid)
-    defpool put(pid, bucket) when is_pid(pid), do: create_search_index(pid, bucket)
-    defpool put(pid, bucket, schema) when is_pid(pid), do: create_search_index(pid, bucket, schema, [])
-    defpool put(pid, bucket, schema, props) when is_pid(pid), do: create_search_index(pid, bucket, schema, props)
-    defpool get(pid, bucket) when is_pid(pid), do: get_search_index(pid, bucket)
-    defpool delete(pid, bucket) when is_pid(pid), do: delete_search_index(pid, bucket)
+    defpool put(pid, index) when is_pid(pid), do: create_search_index(pid, index)
+    defpool put(pid, index, schema) when is_pid(pid), do: create_search_index(pid, index, schema, [])
+    defpool put(pid, index, schema, props) when is_pid(pid), do: create_search_index(pid, index, schema, props)
+    defpool get(pid, index) when is_pid(pid), do: get_search_index(pid, index)
+    defpool delete(pid, index) when is_pid(pid), do: delete_search_index(pid, index)
   end
 
   defmodule Schema do
-    defpool get(pid, bucket) when is_pid(pid), do: get_search_schema(pid, bucket)
+    defpool get(pid, schema) when is_pid(pid), do: get_search_schema(pid, schema)
 
-    defpool create(pid, bucket, content) when is_pid(pid) do
-      create_search_schema(pid, bucket, content)
+    defpool create(pid, schema, content) when is_pid(pid) do
+      create_search_schema(pid, schema, content)
     end
   end
 end

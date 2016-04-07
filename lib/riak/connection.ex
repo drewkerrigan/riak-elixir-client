9d8cdc3c921743e4617f11e45d33b730ea188c57
Olivier <olivier.guizol@intellicore.net>
2016-04-07 20:18:41 +0200

    removed seed in connection init

--- lib/riak/connection.ex
+++ lib/riak/connection.ex
@@ -3,7 +3,6 @@ defmodule Riak.Connection do
   Create a linked process to talk with the riak server on host:port.
   """
   def start_link(host \\ '127.0.0.1', port \\ 8087, args \\ []) do
-    init_seed()
     :riakc_pb_socket.start_link(host, port, args)
   end
 
@@ -11,13 +10,6 @@ defmodule Riak.Connection do
   Create a process to talk with the riak server on host:port.
   """
   def start(host \\ '127.0.0.1', port \\ 8087, args \\ []) do
-    init_seed()
     :riakc_pb_socket.start(host, port, args)
-  end
-
-  defp init_seed do
-    # Let's initialize a seed for pseudo uniform random number - no periodic
-    << a :: 32, b :: 32, c :: 32 >> = :crypto.rand_bytes(12)
-    :random.seed(a, b, c)
   end
 end

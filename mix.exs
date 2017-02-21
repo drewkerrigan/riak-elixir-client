260ac1a23b7442d097c336ec7a523ae884c78950
drewkerrigan <drew@kerrigan.io>
2017-02-21 13:19:16 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Riak.Mixfile do
 
   def project do
     [ app: :riak,
-      version: "1.1.0",
+      version: "1.1.1",
       build_per_environment: false,
       name: "Riak Elixir Client",
       source_url: "https://github.com/drewkerrigan/riak-elixir-client",
